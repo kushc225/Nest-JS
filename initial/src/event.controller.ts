476c37e5808eeae64e36e8138d9b5a6a0e7a935f
kush <kushc225@gmail.com>
2024-07-07 11:59:31 +0530

    more function added to route events/practice

--- initial/src/event.controller.ts
+++ initial/src/event.controller.ts
@@ -11,10 +11,11 @@ import {
   Res,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { Event } from './event.entity';
 import { CreateEventDTO } from './create.event.dto';
 import { UpdatedCreateEventDTO } from './update.event.dto';
+import { skip } from 'node:test';
 @Controller('/events')
 export class EventsController {
   constructor(
@@ -26,21 +27,30 @@ export class EventsController {
   async findAll() {
     return await this.repository.find();
   }
+  @Get('/practice')
+  async pathAll() {
+    return await this.repository.find({
+      select: ['id', 'address'],
+      where: [{description : Like("%world%")}],
+      take: 2,
+      skip: 2,
+
+    });
+  }
 
   @Get(':id')
   async findOne(@Param('id') id) {
-    return await this.repository.findOneBy({id});
+    return await this.repository.findOneBy({ id });
   }
 
   @Post()
   async create(@Body() input: CreateEventDTO) {
     try {
-      
-      const data =  await this.repository.save({
-        ...input, 
+      const data = await this.repository.save({
+        ...input,
         when: new Date(),
       });
-      console.log({data})
+      console.log({ data });
       return data;
     } catch (error) {
       console.log(error.message);
@@ -50,20 +60,21 @@ export class EventsController {
 
   @Patch(':id')
   async update(@Param('id') id, @Body() input: UpdatedCreateEventDTO) {
-    const event = await this.repository.findOneBy({id});
+    const event = await this.repository.findOneBy({ id });
 
     return await this.repository.save({
       ...event,
       ...input,
-      when:new Date(),
+      when: new Date(),
     });
   }
 
   @Delete(':id')
   @HttpCode(204)
   async remove(@Param('id') id) {
-    const event = await this.repository.findOneBy({id});
-    const data =  await this.repository.remove(event);
+    const event = await this.repository.findOneBy({ id });
+    const data = await this.repository.remove(event);
     return data;
   }
+
 }
